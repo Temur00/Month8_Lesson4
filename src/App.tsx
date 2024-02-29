c1d864d413132a11c47556e2f4894d5f30a77086
Temur <shernazarovtemurcom001@gmail.com>
2024-02-29 13:31:51 +0500

    the url is fixed

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Header, MySidebar } from "./components/index";
+import {
+  Dashboard,
+  Login,
+  NotFound,
+  Profile,
+  Register,
+  Students,
+  Teachers,
+} from "./pages/index";
+
+const App = () => {
+  return (
+    <div className="max-h-screen">
+      <Router>
+        <Header />
+        <div className="flex">
+          <MySidebar />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="students" element={<Students />} />
+            <Route path="teachers" element={<Teachers />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
